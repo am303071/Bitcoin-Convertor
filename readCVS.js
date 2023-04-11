f42d8bf73e4d06ba42c6556d4b3efa09803e2b8d
am303071 <107723631+am303071@users.noreply.github.com>
2023-04-11 16:27:30 +0330

    Add files via upload

---
+++ readCVS.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const { parse } = require ('csv-parse');
+
+
+fs.createReadStream("./bitcoinhistory.csv")
+  .pipe(parse({ delimiter: ",", from_line: 2 }))
+  .on("data", function (row) {
+    console.log(row);
+  })
+ 
+  .on("end", function () {
+    console.log("finished");
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  });
